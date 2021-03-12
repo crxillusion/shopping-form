e85732d62f25d53bd0d6c42163abf7fa775b39a2
Kim <kimtorosyan@gmail.com>
2021-03-12 21:08:05 +0400

    fixed warnings

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
-let btn = document.querySelector("button");
+let btns = document.querySelectorAll(".btn");
+let inputs = document.querySelectorAll(".input-required");
 
-btn.onclick = function () {
-    let input = document.querySelector(".shipping-info [name='full-name']");
-    if (input != null) {
-        let val = input.value;
-        if (val.length === 0) {
-            let tooltip = document.querySelector(".shipping-info [name='full-name'] + .input-tooltip");
-            tooltip.classList.add("visible");
+btns.forEach(function (b, i) {
+    b.addEventListener('click', btnClickHandler);
+});
+
+inputs.forEach(function (inp) {
+    inp.addEventListener('input', function () {
+        inp.classList.remove('input-warning');
+        let tooltip = inp.closest(".input-container").querySelector('.input-tooltip');
+        if (tooltip) {
+            tooltip.classList.remove("visible");
         }
+    })
+})
+
+
+function btnClickHandler(e) {
+    e.preventDefault();
+    let slide = this.closest('.form-slide');
+    let inputs = slide.querySelectorAll(".input-required");
+    inputs.forEach(function (inp, ind) {
+        let val = inp.value;
+        if (val.length === 0) {
+            inp.classList.add('input-warning');
+        } else {
+            inp.classList.remove('input-warning');
+        }
+    })
+
+    let firstEmpty = slide.querySelector('.input-warning');
+    let tooltip = firstEmpty.closest(".input-container").querySelector('.input-tooltip');
+    if (tooltip) {
+        tooltip.classList.add("visible");
     }
+
+
 }
+
